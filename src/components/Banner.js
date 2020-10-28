956546dda6d78a1c28272e5b363bf1168732b1f5
Masayuki Ariki <ariki1068@gmail.com>
2020-10-29 07:54:50 +0900

    トップページのサプタイトルに日本語を表示

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = (props) => (
                 <h1>Kurako Rugby 2020</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
+                <p>岡山県立倉敷工業高等学校　ラグビー部　2020年度 A responsive site template designed by HTML5 UP<br />
                 and released under the Creative Commons.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
