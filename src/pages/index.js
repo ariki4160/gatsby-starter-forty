23ea46c6f5c9a7b49cbbdb73591d6d37c1001111
Masayuki Ariki <ariki1068@gmail.com>
2020-10-29 17:57:40 +0900

    杉野太陽君をトップページに追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
-                                <h3>Aliquam</h3>
-                                <p>Ipsum dolor sit amet</p>
+                                <h3>Taiyo Sugino</h3>
+                                <p>captain</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
