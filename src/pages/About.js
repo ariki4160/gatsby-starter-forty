67b80d75a773c319112798afd64d38e378173cb9
Masayuki Ariki <ariki1068@gmail.com>
2020-11-11 07:57:24 +0900

    各所の微調整

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ import pic11 from '../assets/images/pic11.jpg'
 const Generic = (props) => (
     <Layout>
         <Helmet>
-            <title>About This Site</title>
+            <title>About</title>
             <meta name="description" content="Generic Page" />
         </Helmet>
 
@@ -15,11 +15,11 @@ const Generic = (props) => (
             <section id="one">
                 <div className="inner">
                     <header className="major">
-                        <h1>About This Site</h1>
+                        <h1>About</h1>
                     </header>
                     <span className="image main"><img src={pic11} alt="" /></span>
-                    <p>このサイトは、2020年度 岡山県立倉敷工業高等学校 ラグビー部 杉野チーム 卒部記念の特設サイトです。</p>
-                    <p>写真は、宗岡さん、宍戸さん、岡山県ラグビー写真倶楽部さんより提供いただきました。動画、サイト制作は有木が担当いたしました。</p>
+                    <p>このサイトは、2020年度　岡山県立倉敷工業高等学校　ラグビー部　杉野チーム　卒部記念の特設サイトです。</p>
+                    <p>写真は、宗岡さん、宍戸さん、岡山県ラグビー写真倶楽部さんより提供いただきました。</p>
                     <p></p>
                 </div>
             </section>
