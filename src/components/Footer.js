73d7586c852ca51b0c2d26ee21c70b2c8f741b4e
Unknown <ariki1068@gmail.com>
2020-11-15 17:33:47 +0900

    Aboutとフッタを修正

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="copyright">
-                <li>&copy; Kurako Rugby 2020 Team SUGINO Graduation Memorial Website Project</li><li>Photo: Muneoka, Shishido, Okayama Rugby Photography Club</li><li>Movie: Ariki</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+                <li>&copy; Kurako Rugby 2020 Team SUGINO Graduation Memorial Website Project</li><li>Photo: Muneoka, Shishido, Okayama Rugby Photography Club,Ariki</li><li>Movie/Web: Ariki</li><li>Design: <a href="https://html5up.net" target="_blank">HTML5 UP</a></li>
             </ul>
         </div>
     </footer>
